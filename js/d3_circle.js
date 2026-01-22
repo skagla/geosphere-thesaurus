e9fd804f3818458b0c0941108dec734be3618a5a
Johannes Hucek <johannes.hucek@geosphere.at>
2026-01-22 16:03:32 +0100

    test: change echarts_circle layout

--- js/d3_circle.js
+++ js/d3_circle.js
@@ -1,156 +1,182 @@
 ﻿circleChart = function (data) {
-    // Specify the chart’s dimensions.
-    let stack = [];
-    const width = 928;
-    const height = width;
+  // Specify the chart’s dimensions.
+  let stack = [];
+  const width = 928;
+  const height = width;
 
-    // Create the color scale.
-    const color = d3.scaleLinear()
-        .domain([0, 5])
-        .range(["hsl(32,80%,80%)", "hsl(128,30%,40%)"])
-        .interpolate(d3.interpolateHcl);
+  // Create the color scale.
+  const color = d3
+    .scaleLinear()
+    .domain([0, 5])
+    .range(["hsl(32,80%,80%)", "hsl(128,30%,40%)"])
+    // .range(["hsla(212, 89%, 22%, 1.00)", "hsla(209, 98%, 65%, 1.00)"])
+    .interpolate(d3.interpolateHcl);
 
-    // Compute the layout.
-    const pack = data => d3.pack()
-        .size([width, height])
-        .padding(3)
-        (d3.hierarchy(data)
-            .sum(d => d.value)
-            .sort((a, b) => b.value - a.value));
-    let root = pack(data);
+  // Compute the layout.
+  const pack = (data) =>
+    d3.pack().size([width, height]).padding(3)(
+      d3
+        .hierarchy(data)
+        .sum((d) => d.value)
+        .sort((a, b) => b.value - a.value),
+    );
+  let root = pack(data);
 
-    // Create the SVG container.
-    const svg = d3.create("svg")
-        .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
-        .attr("width", "100%")
-        .attr("height", "100%")
-        .attr("style", `font-size: 16px; margin: 0; background: ${root.data.color}; cursor: pointer;`);
+  // Create the SVG container.
+  const svg = d3
+    .create("svg")
+    .attr("viewBox", `-${width / 2} -${height / 2} ${width} ${height}`)
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr(
+      "style",
+      `font-size: 16px; margin: 0; background: ${root.data.color}; cursor: pointer;`,
+    );
 
-    const gNode = svg.append("g")
-        .attr("fill", "#fff")
-        .attr("stroke-opacity", 0.4)
-        .attr("stroke-width", 1.5);
+  const gNode = svg
+    .append("g")
+    .attr("fill", "#fff")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5);
 
-    const nodes = root.descendants();
-    const node = gNode.selectAll("g")
-        .data(nodes, d => d.id);
+  const nodes = root.descendants();
+  const node = gNode.selectAll("g").data(nodes, (d) => d.id);
 
-    function drawStack(d) {
-        let text = null;
-        for (let i = 0; i < stack.length; i++) {
-            text = text != null ? text += " | " : "";
-            text += nodeText(stack[i].data.name);
-        }
-        if (d) {
-            text = text != null ? text += " | " : "";
-            text += nodeText(d.data.name);
-        }
-        document.getElementById("stackContent").innerHTML = text;
+  function drawStack(d) {
+    let text = null;
+    for (let i = 0; i < stack.length; i++) {
+      text = text != null ? (text += " | ") : "";
+      text += nodeText(stack[i].data.name);
     }
-
-
-    function createElements(node) {
-        // Enter any new nodes at the parent's previous position.
-        const nodeEnter = node.enter().append("g")
-            .attr("id", d => "g" + d.data.id)
-            .style("display", d => d.parent === root ? "inline" : "none")
-            ;
-
-        nodeEnter.append("title").html(d => `<p class="title">${d.data.name}</p>`);
-
-        nodeEnter.append("circle")
-            .attr("class", "shadow")
-            .attr("stroke", "#000")
-            .attr("fill", d => d.data.color);
-
-        nodeEnter.append("text")
-            .attr("text-anchor", "middle")
-            .attr("text-rendering", "optimizeLegibility")
-            .text(d => nodeText(d.data.name))
-            .attr("fill", d => d.data.c.length > 0 ? "#2020ff" : "grey")
-            .attr("style", d => d.data.c.length > 0 ? "text-decoration: underline;cursor:pointer;" : "cursor: default;")
-            .attr("paint-order", "stroke")
-            .on("click", (event, d) => {
-                if (d.children && d.parent === focus) {
-                    stack.push(focus);
-                    focus !== d && (zoom(event, d.data.id, gNode.selectAll("g")), event.stopPropagation());
-                }
-            });
-    };
-
-    createElements(node);
-
-    // Create the zoom behavior and zoom immediately in to the initial focus node.
-    svg.on("click", (event) => {
-        let e = stack.pop() || root;
-        zoom(event, e.data.id, gNode.selectAll("g"));
-    });
-    let focus = root;
-    let view;
-    zoomTo([focus.x, focus.y, focus.r * 2], gNode.selectAll("g"));
-    drawStack(root);
-
-    function nodeText(text) {
-        if (text.startsWith("https://") && text.length > 20) {
-            return text.substring(0, 20) + "...";
-        }
-        return text;
+    if (d) {
+      text = text != null ? (text += " | ") : "";
+      text += nodeText(d.data.name);
     }
-    function getById(e, id) {
-        if (e.data.id === id) {
-            return e;
+    document.getElementById("stackContent").innerHTML = text;
+  }
+
+  function createElements(node) {
+    // Enter any new nodes at the parent's previous position.
+    const nodeEnter = node
+      .enter()
+      .append("g")
+      .attr("id", (d) => "g" + d.data.id)
+      .style("display", (d) => (d.parent === root ? "inline" : "none"));
+    nodeEnter
+      .append("title")
+      .html((d) => `<p class="title">${d.data.name}</p>`);
+
+    nodeEnter
+      .append("circle")
+      .attr("class", "shadow")
+      .attr("stroke", "#000")
+      .attr("fill", (d) => d.data.color);
+
+    nodeEnter
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("text-rendering", "optimizeLegibility")
+      .text((d) => nodeText(d.data.name))
+      .attr("fill", (d) => (d.data.c.length > 0 ? "#2020ff" : "grey"))
+      .attr("style", (d) =>
+        d.data.c.length > 0 ?
+          "text-decoration: underline;cursor:pointer;"
+        : "cursor: default;",
+      )
+      .attr("paint-order", "stroke")
+      .on("click", (event, d) => {
+        if (d.children && d.parent === focus) {
+          stack.push(focus);
+          focus !== d &&
+            (zoom(event, d.data.id, gNode.selectAll("g")),
+            event.stopPropagation());
         }
-        if (!e.children) {
-            return null;
-        };
-        for (let i = 0; i < e.children.length; i++) {
-            let d = getById(e.children[i], id);
-            if (d) {
-                return d;
-            }
-        }
-        return null;
+      });
+  }
+
+  createElements(node);
+
+  // Create the zoom behavior and zoom immediately in to the initial focus node.
+  svg.on("click", (event) => {
+    let e = stack.pop() || root;
+    zoom(event, e.data.id, gNode.selectAll("g"));
+  });
+  let focus = root;
+  let view;
+  zoomTo([focus.x, focus.y, focus.r * 2], gNode.selectAll("g"));
+  drawStack(root);
+
+  function nodeText(text) {
+    if (text.startsWith("https://") && text.length > 20) {
+      return text.substring(0, 20) + "...";
     }
-    function zoomTo(v, gNode) {
-        const k = (width / v[2]);
+    return text;
+  }
+  function getById(e, id) {
+    if (e.data.id === id) {
+      return e;
+    }
+    if (!e.children) {
+      return null;
+    }
+    for (let i = 0; i < e.children.length; i++) {
+      let d = getById(e.children[i], id);
+      if (d) {
+        return d;
+      }
+    }
+    return null;
+  }
+  function zoomTo(v, gNode) {
+    const k = width / v[2];
 
-        view = v;
+    view = v;
 
-        /*
+    /*
         label.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
         node.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
         node.attr("r", d => d.r * k);
         */
-        gNode.attr("transform", d => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`);
-        gNode.selectAll("circle").attr("r", d => d.r * k);
-    }
+    gNode.attr(
+      "transform",
+      (d) => `translate(${(d.x - v[0]) * k},${(d.y - v[1]) * k})`,
+    );
+    gNode.selectAll("circle").attr("r", (d) => d.r * k);
+  }
 
-    function zoom(event, id, gNode) {
-        let d = getById(root, id);
-        const focus0 = focus;
+  function zoom(event, id, gNode) {
+    let d = getById(root, id);
+    const focus0 = focus;
 
-        focus = d;
+    focus = d;
 
-        const transition = svg.transition()
-            .duration(event.altKey ? 7500 : 750)
-            .tween("zoom", d => {
-                const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
-                return t => zoomTo(i(t), gNode);
-            });
+    const transition = svg
+      .transition()
+      .duration(event.altKey ? 7500 : 750)
+      .tween("zoom", (d) => {
+        const i = d3.interpolateZoom(view, [focus.x, focus.y, focus.r * 2]);
+        return (t) => zoomTo(i(t), gNode);
+      });
 
-        gNode
-            .filter(function (d) { return d.parent === focus || this.style.display === "inline"; })
-            .transition(transition)
-            .style("fill-opacity", d => d.parent === focus ? 1 : 0)
-            .on("start", function (d) { if (d.parent === focus) this.style.display = "inline"; })
-            .on("end", function (d) { if (d.parent !== focus) this.style.display = "none"; });
+    gNode
+      .filter(function (d) {
+        return d.parent === focus || this.style.display === "inline";
+      })
+      .transition(transition)
+      .style("fill-opacity", (d) => (d.parent === focus ? 1 : 0))
+      .on("start", function (d) {
+        if (d.parent === focus) this.style.display = "inline";
+      })
+      .on("end", function (d) {
+        if (d.parent !== focus) this.style.display = "none";
+      });
 
-        svg.style("background", focus.data.color);
-        drawStack(d);
-    }
+    svg.style("background", focus.data.color);
+    drawStack(d);
+  }
 
-    return svg.node();
-}
+  return svg.node();
+};
 
 /*
 d3data.init(function (data) {
--- js/echarts_circle.js
+++ js/echarts_circle.js
@@ -1,235 +1,235 @@
 ﻿eCharts_circleChart = function (rawData, elementId) {
-    let chartDom = document.getElementById(elementId);
+  let chartDom = document.getElementById(elementId);
 
-    let myChart = echarts.init(chartDom, null, {
-        renderer: 'svg'
-    });
-    function outputsize() {
-        setTimeout(function () {
-            myChart.resize();
-        }, 10);
+  let myChart = echarts.init(chartDom, null, {
+    renderer: "svg",
+  });
+  function outputsize() {
+    setTimeout(function () {
+      myChart.resize();
+    }, 10);
+  }
+  new ResizeObserver(outputsize).observe(chartDom);
+
+  var option;
+  function prepareData(rawData) {
+    const seriesData = [];
+    let maxDepth = 0;
+    let ix = 0;
+    function convert(source, parent, depth) {
+      if (source == null) {
+        return;
+      }
+      if (maxDepth > 5) {
+        return;
+      }
+      maxDepth = Math.max(depth, maxDepth);
+      source.id = ix;
+
+      ix++;
+      let nd = {
+        id: source.id,
+        name: source.name,
+        parentId: parent != null ? parent.id : null,
+        value: source.value,
+        depth: depth,
+        index: seriesData.length,
+        itemStyle: source.itemStyle,
+      };
+      seriesData.push(nd);
+      if (source.children && source.children.length > 0) {
+        for (let c of source.children) {
+          convert(c, source, depth + 1);
+        }
+      } else if (!nd.value) {
+        nd.value = 1;
+      }
     }
-    new ResizeObserver(outputsize).observe(chartDom);
-
-    var option;
-    function prepareData(rawData) {
-        const seriesData = [];
-        let maxDepth = 0;
-        let ix = 0;
-        function convert(source, parent, depth) {
-            if (source == null) {
-                return;
-            }
-            if (maxDepth > 5) {
-                return;
-            }
-            maxDepth = Math.max(depth, maxDepth);
-            source.id = ix;
-
-            ix++;
-            let nd = {
-                id: source.id,
-                name: source.name,
-                parentId: parent != null ? parent.id : null,
-                value: source.value,
-                depth: depth,
-                index: seriesData.length,
-                itemStyle: source.itemStyle
-            };
-            seriesData.push(nd);
-            if (source.children && source.children.length > 0) {
-                for (let c of source.children) {
-                    convert(c, source, depth + 1);
-                }
-            } else if (!nd.value) {
-                nd.value = 1;
-            }
-        }
-        convert(rawData, null, 0);
-        return {
-            seriesData: seriesData,
-            maxDepth: maxDepth
-        };
+    convert(rawData, null, 0);
+    return {
+      seriesData: seriesData,
+      maxDepth: maxDepth,
+    };
+  }
+  var all;
+  function initChart(seriesData, maxDepth) {
+    var displayRoot = stratify();
+    all = displayRoot.descendants();
+    function stratify() {
+      return d3
+        .stratify()
+        .parentId(function (d) {
+          return d.parentId;
+        })(seriesData)
+        .sum(function (d) {
+          return d.value || 1;
+        })
+        .sort(function (a, b) {
+          return b.value - a.value;
+        });
     }
-    var all;
-    function initChart(seriesData, maxDepth) {
-        var displayRoot = stratify();
-        all = displayRoot.descendants();
-        function stratify() {
-            return d3
-                .stratify()
-                .parentId(function (d) {
-                    return d.parentId;
-                })(seriesData)
-                .sum(function (d) {
-                    return d.value || 1;
-                })
-                .sort(function (a, b) {
-                    return b.value - a.value;
-                });
-        }
-        function overallLayout(params, api) {
-            var context = params.context;
-            d3
-                .pack()
-                .size([api.getWidth() - 2, api.getHeight() - 2])
-                .padding(3)(displayRoot);
-            context.nodes = {};
-            displayRoot.descendants().forEach(function (node, index) {
-                context.nodes[node.id] = node;
-            });
-        }
-        function renderItem(params, api) {
-            var context = params.context;
-            // Only do that layout once in each time `setOption` called.
-            if (!context.layout) {
-                context.layout = true;
-                overallLayout(params, api);
-            }
-            var nodePath = api.value('id');
-            var node = context.nodes[nodePath];
-            if (!node) {
-                // Reder nothing.
-                return;
-            }
-            var isLeaf = !node.children || !node.children.length;
-            var focus = new Uint32Array(
-                node.descendants().map(function (node) {
-                    return node.data.index;
-                })
-            );
-            var nodeName = node.data.name;
-            var z2 = api.value('depth') * 2;
+    function overallLayout(params, api) {
+      var context = params.context;
+      d3
+        .pack()
+        .size([api.getWidth() - 2, api.getHeight() - 2])
+        .padding(3)(displayRoot);
+      context.nodes = {};
+      displayRoot.descendants().forEach(function (node, index) {
+        context.nodes[node.id] = node;
+      });
+    }
+    function renderItem(params, api) {
+      var context = params.context;
+      // Only do that layout once in each time `setOption` called.
+      if (!context.layout) {
+        context.layout = true;
+        overallLayout(params, api);
+      }
+      var nodePath = api.value("id");
+      var node = context.nodes[nodePath];
+      if (!node) {
+        // Reder nothing.
+        return;
+      }
+      var isLeaf = !node.children || !node.children.length;
+      var focus = new Uint32Array(
+        node.descendants().map(function (node) {
+          return node.data.index;
+        }),
+      );
+      var nodeName = node.data.name;
+      var z2 = api.value("depth") * 2;
 
-            let itemColor = api.visual('color');
+      let itemColor = api.visual("color");
 
-            drawStack(displayRoot);
+      drawStack(displayRoot);
 
-            return {
-                type: 'circle',
-                focus: focus,
-                shape: {
-                    cx: node.x,
-                    cy: node.y,
-                    r: node.r
-                },
-                transition: ['shape'],
-                z2: z2,
-                textContent: {
-                    type: 'text',
-                    style: {
-                        // transition: isLeaf ? 'fontSize' : null,
-                        text: nodeName.replace(/ /g, '\n'),
-                        width: node.r * 1.3,
-                        overflow: 'truncate',
-                        fontSize: 12,
-                        color: 'grey'
-                    },
-                    emphasis: {
-                        style: {
-                            overflow: null,
-                            fontSize: 28,
-                            color: 'grey'
-                        }
-                    }
-                },
-                textConfig: {
-                    position: 'inside'
-                },
-                style: {
-                    fill: (node.data.itemStyle.color || api.visual('color'))
-                },
-                emphasis: {
-                    style: {
-                        fontSize: 12,
-                        shadowBlur: 20,
-                        shadowOffsetX: 3,
-                        shadowOffsetY: 5,
-                        shadowColor: 'rgba(0,0,0,0.3)'
-                    }
-                }
-            };
-        }
-        function nodeText(text) {
-            if (text.startsWith("https://") && text.length > 20) {
-                return text.substring(0, 20) + "...";
-            }
-            return text;
-        }
-        function drawStack(node) {
-            let text = null;
-            while (node) {
-                text = text != null ? " | " + text : "";
-                text = nodeText(node.data.name) + text;
-                let parentId = node.data.parentId;
-                node = all.find(function (n) {
-                    return n.data.id === parentId;
-                });
-            }
-            document.getElementById("stackContent").innerHTML = text;
-        }
-        option = {
-            dataset: {
-                source: seriesData
+      return {
+        type: "circle",
+        focus: focus,
+        shape: {
+          cx: node.x,
+          cy: node.y,
+          r: node.r,
+        },
+        transition: ["shape"],
+        z2: z2,
+        textContent: {
+          type: "text",
+          style: {
+            // transition: isLeaf ? "fontSize" : null,
+            text: nodeName.replace(/ /g, "\n"),
+            width: node.r * 1.3,
+            overflow: "truncate",
+            // fontSize: 12,
+            fontSize: isLeaf ? node.r / 3 : 0,
+            color: "grey",
+          },
+          emphasis: {
+            style: {
+              overflow: null,
+              //   fontSize: 28,
+              fontSize: isLeaf ? Math.max(node.r / 3, 12) : 0,
+              color: "grey",
             },
-            tooltip: {},
-            visualMap: [
-                {
-                    show: false,
-                    min: 0,
-                    max: maxDepth,
-                    dimension: 'depth',
-                    inRange: {
-                        color: ['#006edd', '#e0ffff']
-                    }
-                }
-            ],
-            hoverLayerThreshold: Infinity,
-            series: {
-                type: 'custom',
-                renderItem: renderItem,
-                progressive: 0,
-                coordinateSystem: 'none',
-                encode: {
-                    tooltip: 'name'
-                }
-            }
-        };
-        myChart.setOption(option);
-        myChart.on('click', { seriesIndex: 0 }, function (params) {
-            drillDown(params.data.id);
-        });
-        function drillDown(targetNodeId) {
-            let isTargetNodeId = targetNodeId != null;
-            let dr = displayRoot;
-            displayRoot = stratify();
-            if (targetNodeId == null) {
-                if (dr.data.parentId != null)
-                    targetNodeId = dr.data.parentId;
-            }
-            if (targetNodeId != null) {
-                displayRoot = displayRoot.descendants().find(function (node) {
-                    return node.data.id === targetNodeId;
-                });
-            }
-            // A trick to prevent d3-hierarchy from visiting parents in this algorithm.
-            drawStack(displayRoot);
-            displayRoot.parent = null;
-            myChart.setOption({
-                dataset: {
-                    source: seriesData
-                }
-            });
-        }
-        // Reset: click on the blank area.
-        myChart.getZr().on('click', function (event) {
-            if (!event.target) {
-                drillDown();
-            }
-        });
+          },
+        },
+        textConfig: {
+          position: "inside",
+        },
+        style: {
+          fill: node.data.itemStyle.color || api.visual("color"),
+        },
+        emphasis: {
+          style: {
+            fontSize: 12,
+            shadowBlur: 20,
+            shadowOffsetX: 3,
+            shadowOffsetY: 5,
+            shadowColor: "rgba(0,0,0,0.3)",
+          },
+        },
+      };
     }
+    function nodeText(text) {
+      if (text.startsWith("https://") && text.length > 20) {
+        return text.substring(0, 20) + "...";
+      }
+      return text;
+    }
+    function drawStack(node) {
+      let text = null;
+      while (node) {
+        text = text != null ? " | " + text : "";
+        text = nodeText(node.data.name) + text;
+        let parentId = node.data.parentId;
+        node = all.find(function (n) {
+          return n.data.id === parentId;
+        });
+      }
+      document.getElementById("stackContent").innerHTML = text;
+    }
+    option = {
+      dataset: {
+        source: seriesData,
+      },
+      tooltip: {},
+      visualMap: [
+        {
+          show: false,
+          min: 0,
+          max: maxDepth,
+          dimension: "depth",
+          inRange: {
+            color: ["#006edd", "#e0ffff"],
+          },
+        },
+      ],
+      hoverLayerThreshold: Infinity,
+      series: {
+        type: "custom",
+        renderItem: renderItem,
+        progressive: 0,
+        coordinateSystem: "none",
+        encode: {
+          tooltip: "name",
+        },
+      },
+    };
+    myChart.setOption(option);
+    myChart.on("click", { seriesIndex: 0 }, function (params) {
+      drillDown(params.data.id);
+    });
+    function drillDown(targetNodeId) {
+      let isTargetNodeId = targetNodeId != null;
+      let dr = displayRoot;
+      displayRoot = stratify();
+      if (targetNodeId == null) {
+        if (dr.data.parentId != null) targetNodeId = dr.data.parentId;
+      }
+      if (targetNodeId != null) {
+        displayRoot = displayRoot.descendants().find(function (node) {
+          return node.data.id === targetNodeId;
+        });
+      }
+      // A trick to prevent d3-hierarchy from visiting parents in this algorithm.
+      drawStack(displayRoot);
+      displayRoot.parent = null;
+      myChart.setOption({
+        dataset: {
+          source: seriesData,
+        },
+      });
+    }
+    // Reset: click on the blank area.
+    myChart.getZr().on("click", function (event) {
+      if (!event.target) {
+        drillDown();
+      }
+    });
+  }
 
-    const dataWrap = prepareData(rawData);
-    initChart(dataWrap.seriesData, dataWrap.maxDepth);
-
-}
+  const dataWrap = prepareData(rawData);
+  initChart(dataWrap.seriesData, dataWrap.maxDepth);
+};
